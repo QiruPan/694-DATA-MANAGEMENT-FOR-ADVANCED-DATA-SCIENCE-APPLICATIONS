e34da1aa72eda34abcc414a60ee10e90bd9377c9
Qiru Pan <121769530+QiruPan@users.noreply.github.com>
2023-04-11 23:17:40 -0400

    Graphical User Interface for searching engine

---
+++ GUI.py
@@ -0,0 +1,37 @@
+# You must provide several options such as search by string, hashtag,  and user at the minimum.
+from pymongo import MongoClient
+import mysql.connector
+print("Please enter a search term in string, hashtag,  and user: ")
+search_term = input()
+if search_term == "string":
+    print("Please enter a string: ")
+    string = input()
+    
+    client = MongoClient('localhost', 27017)
+    db = client['tweet']
+    collection_currency = db['tweet']
+    myquery = { "text": string }
+    mydoc = collection_currency.find(myquery)
+    for x in mydoc:
+        print(x)
+elif search_term == "hashtag":
+    print("Please enter a hashtag: ")
+    hashtag = input()
+    client = MongoClient('localhost', 27017)
+    db = client['tweet']
+    collection_currency = db['tweet']
+    print("there are no hashtag in the dataset so we do not search.")
+elif search_term == "user":
+    print("Please enter a user name: ")
+    user = input()
+    mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="135790"
+    )
+
+    mycursor = mydb.cursor()
+    mycursor.execute('SELECT * FROM pqr_user.tusers WHERE name = "'+user+'"')
+    myresult = mycursor.fetchall()
+    for x in myresult:
+        print(x)
