205da2c98ad1b257762b3cbb9e7253c918cb324c
Qiru Pan <121769530+QiruPan@users.noreply.github.com>
2023-04-11 23:30:45 -0400

    Transforming data into json document

---
+++ Preprocesses.py
@@ -0,0 +1,20 @@
+import os
+
+# Python program to read
+# json file
+  
+  
+import json
+  
+# Opening JSON file
+f = open('data.json')
+  
+# returns JSON object as 
+# a dictionary
+data = json.load(f)
+  
+# Iterating through the json
+# list
+for i in data:
+    print(i)
+    break
